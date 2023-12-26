42971fad4b24eae767d51c578f5856b3619e0ebd
Julian Pömp <jp@poemp.net>
2023-12-26 09:56:42 +0100

    build(project): fix project files

--- packages/clusterfy/jest.config.ts
+++ packages/clusterfy/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'clusterfy',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../coverage/clusterfy',
+  coverageDirectory: '../../coverage/packages/clusterfy',
 };
